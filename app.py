1cfa69b1a5334321ee9e41166447307915930d0d
moses197 <leomoszeko@gmail.com>
2023-02-19 00:10:50 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact_page.html")
+
+@app.route("/")
+def project():
+    return render_template("project_page.html")
+
+if __name__ ==' __main__':
+    app.run(debug = True)
